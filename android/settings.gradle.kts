408939e5c24f13886de6d11f508d9093b6b68118
Builder <build@aijia.tv>
2026-03-12 19:36:21 +0800

    修复: 添加FLUTTER_ROOT环境变量支持，修复CI构建

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,14 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = file("local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.inputStream().use { properties.load(it) }
         }
+        properties.getProperty("flutter.sdk")
+            ?: System.getenv("FLUTTER_ROOT")
+            ?: throw GradleException("Flutter SDK not found. Define flutter.sdk in local.properties or set FLUTTER_ROOT environment variable.")
+    }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
